ccc800a7298598722ddcdf58df2d41cb05576a94
Jimmy Engström <jimmy@apeoholic.se>
2020-05-27 00:07:04 +0200

    Fixed error in Export caused by null value

--- Blazm.Components/SampleServerSide/Data/WeatherForecast.cs
+++ Blazm.Components/SampleServerSide/Data/WeatherForecast.cs
@@ -13,5 +13,6 @@ namespace SampleServerSide.Data
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
+        public string NullValue { get; set; } = null;
     }
 }
