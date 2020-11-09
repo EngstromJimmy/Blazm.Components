a8af5f12a3c2e9717de31cd9a5cd6e5c884b3462
Jimmy Engström <jimmy@apeoholic.se>
2020-11-09 22:59:59 +0100

    Updated to .NET 5  and added NullGridTemplate

--- Blazm.Components/Blazm.Components/Extensions/NavigationManagerExtensions.cs
+++ Blazm.Components/Blazm.Components/Extensions/NavigationManagerExtensions.cs
@@ -5,9 +5,12 @@ namespace Microsoft.AspNetCore.Components
 {
     public static class NavigationManagerExtensions
     {
+        static IJSObjectReference windowmodule;
+
         public static async Task NavigateToNewWindowAsync(this NavigationManager navigator, IJSRuntime jsRuntime, string url, string content)
         {
-            await jsRuntime.InvokeAsync<object>("NavigationManagerExtensions.openInNewWindow", url, content);
+            windowmodule = await jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/Blazm.Components/scripts/OpenInNewWindow.js");
+            await windowmodule.InvokeAsync<object>("OpenInNewWindow", url, content);
         }
     }
 }
---
+++ Blazm.Components/Blazm.Components/wwwroot/Scripts/SaveAsFile.js
@@ -0,0 +1,20 @@
+﻿export function SaveAsFile(filename, type, bytesBase64) {
+    if (navigator.msSaveBlob) {
+        //Download document in Edge browser
+        var data = window.atob(bytesBase64);
+        var bytes = new Uint8Array(data.length);
+        for (var i = 0; i < data.length; i++) {
+            bytes[i] = data.charCodeAt(i);
+        }
+        var blob = new Blob([bytes.buffer], { type: type });
+        navigator.msSaveBlob(blob, filename);
+    }
+    else {
+        var link = document.createElement('a');
+        link.download = filename;
+        link.href = "data:" + type + ";base64," + bytesBase64;
+        document.body.appendChild(link); // Needed for Firefox
+        link.click();
+        document.body.removeChild(link);
+    }
+}
