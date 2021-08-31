2b7f25c4034936be24afbde357143d65766223cb
Jimmy Engström <jimmy@apeoholic.se>
2021-08-31 15:10:41 +0200

    Added conversion to base64 to work with both .NET5 and .NET6. This will be fixed as soon as .NET 6 is released

--- Blazm.Components/Blazm.Components/Extensions/JSInteropExtensions.cs
+++ Blazm.Components/Blazm.Components/Extensions/JSInteropExtensions.cs
@@ -11,7 +11,7 @@ namespace Microsoft.JSInterop
         public static async Task SaveAsFileAsync(this IJSRuntime js, string filename, byte[] data, string type = "application/octet-stream")
         {
             var savefilemodule = await js.InvokeAsync<IJSObjectReference>("import", "./_content/Blazm.Components/scripts/SaveAsFile.js");
-            await savefilemodule.InvokeVoidAsync("SaveAsFile", filename, type,data);
+            await savefilemodule.InvokeVoidAsync("SaveAsFile", filename, type, Convert.ToBase64String(data));
         }   
     }
 }
